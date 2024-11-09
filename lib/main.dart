8627a720fc87370477b1064b2b49bd5ed8b1f997
Aita8566 <157978621+Aita8566@users.noreply.github.com>
2024-11-09 06:33:34 -0800

    aita

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'test 2 aita',
+      title: 'test 2 aita y',
       theme: ThemeData(
         
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
