47a0cda260fe13e67d1773d7b04856d135e42307
samba1441 <sambabocoum8@gmail.com>
2024-11-09 14:37:28 +0000

    valider

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'test 2 aita y',
+      title: 'test 2 aita bocoum',
       theme: ThemeData(
         
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
