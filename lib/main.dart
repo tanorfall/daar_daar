5af892a703db7cb3fc5cc7f803b2542e90339b3f
Tanor Fall <tanorfall772@gmail.com>
2024-11-09 14:01:27 +0000

    test 2

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'ya salam',
+      title: 'test 2',
       theme: ThemeData(
         
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
