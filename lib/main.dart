9052d0b2f15deddac64567011c7539a8400b07c9
Abdourahamane Dialloo <dialloabdourahamane171@gmail.com>
2024-11-09 14:47:36 +0100

    diallo

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'test 2 aita bocoum',
+      title: 'test 2 aita bocoum diallo',
       theme: ThemeData(
         
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
